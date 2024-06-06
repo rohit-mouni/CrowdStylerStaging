543c90b5e81ec999cccc75bc3e5180f74e57cdcb
rohit-mouni <rohitmounioo7@gmail.com>
2024-06-06 15:37:53 +0530

    new changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    
+
     /**
      * Get the attributes that should be cast.
      *
@@ -46,3 +46,5 @@ class User extends Authenticatable
         ];
     }
 }
+
+
